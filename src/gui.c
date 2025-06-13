041afe1df0a572e53f2b764772c19c4788a41e12
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2025-06-13 12:18:47 +1000

    On-the-fly glyph rasterization

--- src/gui.c
+++ src/gui.c
@@ -27,12 +27,12 @@
 #include <text.glsl.h>
 
 // TODO
+// Bind correct atlas img when drawing text
 // Handle variety of text alignments. eg TL, TC, TR, CL, CC, CR, BL, BC, BR
-// Test mixed language strings
 // Handle proper blending of text so glyphs don't clip each other
 // Handle proper blending of text so subpixel antialiasing blends with background
 // Handle multiple fonts (bold/italic) & font sizes
-// If atlas is full, create new font atlas
+// Test mixed language strings (Try this? https://github.com/Tehreer/SheenBidi)
 // Add ability to clear font atlas on resize
 // Use smaller atlases 128x128 (RGBA 64kb)
 
@@ -100,7 +100,8 @@ enum
     ATLAS_ROW_STRIDE  = ATLAS_WIDTH * 4,
     ATLAS_INT16_SHIFT = 8,
 
-    FONT_SIZE = 12,
+    FONT_SIZE        = 12,
+    RECTPACK_PADDING = 1,
 };
 _Static_assert(MAX_INDICES < UINT16_MAX, "UINT16 Overflow");
 _Static_assert((ATLAS_WIDTH << ATLAS_INT16_SHIFT) == (1 << 15), "");
@@ -129,15 +130,42 @@ typedef struct atlas_rect_t
 } atlas_rect;
 _Static_assert(ATLAS_WIDTH <= UINT8_MAX + 1, "");
 
+typedef struct glyph_atlas_t
+{
+    sg_image img;
+    bool     dirty;
+    bool     full;
+} glyph_atlas;
+
+glyph_atlas glyph_atlas_new()
+{
+    glyph_atlas atlas = {
+        .img = sg_make_image(&(sg_image_desc){
+            .width                = ATLAS_WIDTH,
+            .height               = ATLAS_HEIGHT,
+            .pixel_format         = SG_PIXELFORMAT_RGBA8,
+            .usage.dynamic_update = true,
+        })};
+    xassert(atlas.img.id);
+    return atlas;
+}
+
 typedef struct GUI
 {
     Plugin*      plugin;
     void*        pw;
     _sg_state_t* sg;
 
-    stbrp_context font_atlas;
-    stbrp_node*   nodes;
-    atlas_rect*   rects;
+    glyph_atlas* glyph_atlases;
+    atlas_rect*  rects;
+
+    struct
+    {
+        int            idx;
+        stbrp_context  ctx;
+        stbrp_node*    nodes;
+        unsigned char* img_data;
+    } current_atlas;
 
     FT_Library ft_lib;
     FT_Face    ft_face;
@@ -145,7 +173,6 @@ typedef struct GUI
     sg_pipeline pip;
     sg_buffer   vbo;
     sg_buffer   ibo;
-    sg_image    img;
     sg_sampler  smp;
 
     // struct load_img_t brain;
@@ -154,6 +181,126 @@ typedef struct GUI
     vertex_t vertices[MAX_VERTICES];
     uint16_t indices[MAX_INDICES];
 } GUI;
+
+int raster_glyph(GUI* gui, uint32_t glyph_index)
+{
+    int num_packed = 0;
+
+    xassert(gui->current_atlas.idx < xarr_len(gui->glyph_atlases));
+    glyph_atlas* atlas = gui->glyph_atlases + gui->current_atlas.idx;
+
+    int err = FT_Load_Glyph(gui->ft_face, glyph_index, FT_LOAD_DEFAULT);
+    xassert(!err);
+
+    FT_GlyphSlot glyph = gui->ft_face->glyph;
+
+    FT_Render_Mode render_mode = FT_RENDER_MODE_LCD; // subpixel antialiasing, horizontal screen
+    FT_Render_Glyph(glyph, render_mode);
+
+    const FT_Bitmap* bmp = &glyph->bitmap;
+    xassert(bmp->pixel_mode == FT_PIXEL_MODE_LCD);
+    xassert((bmp->width % 3) == 0); // note: FT width is measured in bytes (subpixels)
+
+    // Note all glyphs have height/rows... (spaces?)
+    if (bmp->width && bmp->rows)
+    {
+        int        width_pixels = bmp->width / 3;
+        stbrp_rect rect         = {.w = width_pixels + RECTPACK_PADDING, .h = bmp->rows + RECTPACK_PADDING};
+        num_packed              = stbrp_pack_rects(&gui->current_atlas.ctx, &rect, 1);
+
+        if (num_packed == 0) // atlas is full
+        {
+            atlas->full = true;
+            sg_update_image(
+                atlas->img,
+                &(sg_image_data){
+                    .subimage[0][0] = {
+                        .ptr  = gui->current_atlas.img_data,
+                        .size = ATLAS_HEIGHT * ATLAS_ROW_STRIDE,
+                    }});
+            atlas->dirty = false;
+
+            // Clear rectpack
+            memset(&gui->current_atlas.ctx, 0, sizeof(gui->current_atlas.ctx));
+            stbrp_init_target(
+                &gui->current_atlas.ctx,
+                ATLAS_WIDTH,
+                ATLAS_HEIGHT,
+                gui->current_atlas.nodes,
+                xarr_len(gui->current_atlas.nodes));
+
+            rect       = (stbrp_rect){.w = width_pixels + RECTPACK_PADDING, .h = bmp->rows + RECTPACK_PADDING};
+            num_packed = stbrp_pack_rects(&gui->current_atlas.ctx, &rect, 1);
+            xassert(num_packed == 1);
+
+            // make new atlas
+            glyph_atlas new_atlas = glyph_atlas_new();
+            xarr_push(gui->glyph_atlases, new_atlas);
+            gui->current_atlas.idx++;
+
+            atlas = gui->glyph_atlases + gui->current_atlas.idx;
+        }
+
+        if (num_packed)
+        {
+            atlas_rect arect;
+            arect.header.glyphid   = glyph_index;
+            arect.header.font_size = FONT_SIZE;
+            arect.pen_offset_x     = glyph->bitmap_left;
+            arect.pen_offset_y     = glyph->bitmap_top;
+            arect.x                = rect.x;
+            arect.y                = rect.y;
+            arect.w                = width_pixels;
+            arect.h                = bmp->rows;
+            arect.atlas            = atlas->img;
+            xassert(arect.x + arect.w <= ATLAS_WIDTH);
+            xassert(arect.y + arect.h <= ATLAS_HEIGHT);
+
+            xarr_push(gui->rects, arect);
+
+            // println("Printing character \"%c\" to atlas at %dx%d", (char)codepoint, rect.x, rect.y);
+
+            for (int y = 0; y < bmp->rows; y++)
+            {
+                unsigned char* dst = gui->current_atlas.img_data + (rect.y + y) * ATLAS_ROW_STRIDE + rect.x * 4;
+                unsigned char* src = bmp->buffer + y * bmp->pitch;
+
+                for (int x = 0; x < width_pixels; x++, dst += 4, src += 3)
+                {
+                    dst[0] = src[0];
+                    dst[1] = src[1];
+                    dst[2] = src[2];
+                    dst[3] = 0xff;
+                }
+            }
+
+            atlas->dirty = true;
+        }
+    }
+
+    return num_packed;
+}
+
+atlas_rect* get_glyph_rect(GUI* gui, uint32_t glyph_index)
+{
+    atlas_rect* rect      = NULL;
+    const int   num_rects = xarr_len(gui->rects);
+    for (int j = 0; j < num_rects; j++)
+    {
+        if (gui->rects[j].header.glyphid == glyph_index)
+        {
+            rect = gui->rects + j;
+            return rect;
+        }
+    }
+    int did_raster = raster_glyph(gui, glyph_index);
+    if (did_raster)
+    {
+        xassert(num_rects + 1 == xarr_len(gui->rects));
+        rect = gui->rects + num_rects;
+    }
+    return rect;
+}
 
 static void my_sg_logger(
     const char* tag,              // always "sapp"
@@ -282,99 +429,43 @@ void* pw_create_gui(void* _plugin, void* _pw)
             .wrap_u     = SG_WRAP_CLAMP_TO_EDGE,
             .wrap_v     = SG_WRAP_CLAMP_TO_EDGE,
         });
-
-        gui->img = sg_make_image(&(sg_image_desc){
-            .width                = ATLAS_WIDTH,
-            .height               = ATLAS_HEIGHT,
-            .pixel_format         = SG_PIXELFORMAT_RGBA8,
-            .usage.dynamic_update = true,
-        });
-
-        xassert(gui->img.id);
     }
 
     int err = FT_Init_FreeType(&gui->ft_lib);
     xassert(!err);
-    err = FT_New_Face(gui->ft_lib, "C:\\Windows\\Fonts\\arialbd.ttf", 0, &gui->ft_face);
+    // const char* font_path = "C:\\Windows\\Fonts\\arialbd.ttf";
+    const char* font_path = "C:\\Windows\\Fonts\\segoeui.ttf";
+    // const char* font_path = "C:\\Windows\\Fonts\\segoeuib.ttf"; // bold
+    // const char* font_path = "C:\\Windows\\Fonts\\seguisb.ttf"; // semibold
+    xassert(xfiles_exists(font_path));
+    err = FT_New_Face(gui->ft_lib, font_path, 0, &gui->ft_face);
     xassert(!err);
     println("Found %ld glyphs", gui->ft_face->num_glyphs);
 
     const float DPI = 96;
     FT_Set_Char_Size(gui->ft_face, 0, FONT_SIZE * 64, DPI, DPI);
 
-    unsigned char* atlas = xcalloc(1, ATLAS_HEIGHT * ATLAS_ROW_STRIDE);
+    gui->current_atlas.idx = 0;
+    xarr_setcap(gui->glyph_atlases, 16);
+    xarr_setlen(gui->glyph_atlases, 1);
+    gui->glyph_atlases[0] = glyph_atlas_new();
 
-    xarr_setlen(gui->nodes, (ATLAS_WIDTH * 2));
-    stbrp_init_target(&gui->font_atlas, ATLAS_WIDTH, ATLAS_HEIGHT, gui->nodes, xarr_len(gui->nodes));
+    xarr_setlen(gui->current_atlas.nodes, (ATLAS_WIDTH * 2));
+    gui->current_atlas.img_data = xcalloc(1, ATLAS_HEIGHT * ATLAS_ROW_STRIDE);
+    stbrp_init_target(
+        &gui->current_atlas.ctx,
+        ATLAS_WIDTH,
+        ATLAS_HEIGHT,
+        gui->current_atlas.nodes,
+        xarr_len(gui->current_atlas.nodes));
 
+    // Pre-render standard latin
     // from "!" to "~" https://www.ascii-code.com/
     for (int codepoint = 33; codepoint < 127; codepoint++)
     {
         FT_UInt glyph_index = FT_Get_Char_Index(gui->ft_face, codepoint);
-        err                 = FT_Load_Glyph(gui->ft_face, glyph_index, FT_LOAD_DEFAULT);
-        xassert(!err);
-
-        FT_GlyphSlot glyph = gui->ft_face->glyph;
-
-        FT_Render_Mode render_mode = FT_RENDER_MODE_LCD; // subpixel antialiasing, horizontal screen
-        FT_Render_Glyph(glyph, render_mode);
-
-        const FT_Bitmap* bmp = &glyph->bitmap;
-        xassert(bmp->pixel_mode == FT_PIXEL_MODE_LCD);
-        xassert((bmp->width % 3) == 0); // note: FT width is measured in bytes (subpixels)
-
-        // Note all glyphs have height/rows... (spaces?)
-        if (bmp->width && bmp->rows)
-        {
-            int        width_pixels = bmp->width / 3;
-            stbrp_rect rect         = {.w = width_pixels + 1, .h = bmp->rows + 1};
-            int        num_packed   = stbrp_pack_rects(&gui->font_atlas, &rect, 1);
-            xassert(num_packed);
-            if (num_packed)
-            {
-                atlas_rect arect;
-                arect.header.glyphid   = glyph_index;
-                arect.header.font_size = FONT_SIZE;
-                arect.pen_offset_x     = glyph->bitmap_left;
-                arect.pen_offset_y     = glyph->bitmap_top;
-                arect.x                = rect.x;
-                arect.y                = rect.y;
-                arect.w                = width_pixels;
-                arect.h                = bmp->rows;
-                arect.atlas            = gui->img;
-                xassert(arect.x + arect.w <= ATLAS_WIDTH);
-                xassert(arect.y + arect.h <= ATLAS_HEIGHT);
-
-                xarr_push(gui->rects, arect);
-
-                // println("Printing character \"%c\" to atlas at %dx%d", (char)codepoint, rect.x, rect.y);
-
-                for (int y = 0; y < bmp->rows; y++)
-                {
-                    unsigned char* dst = atlas + (rect.y + y) * ATLAS_ROW_STRIDE + rect.x * 4;
-                    unsigned char* src = bmp->buffer + y * bmp->pitch;
-
-                    for (int x = 0; x < width_pixels; x++, dst += 4, src += 3)
-                    {
-                        dst[0] = src[0];
-                        dst[1] = src[1];
-                        dst[2] = src[2];
-                        dst[3] = 0xff;
-                    }
-                }
-            }
-        }
+        raster_glyph(gui, glyph_index);
     }
-
-    sg_update_image(
-        gui->img,
-        &(sg_image_data){
-            .subimage[0][0] = {
-                .ptr  = atlas,
-                .size = ATLAS_HEIGHT * ATLAS_ROW_STRIDE,
-            }});
-
-    xfree(atlas);
 
     // img
     // {
@@ -398,8 +489,10 @@ void pw_destroy_gui(void* _gui)
     sg_set_global(gui->sg);
     sg_shutdown(gui->sg);
 
-    xarr_free(gui->nodes);
+    xfree(gui->current_atlas.img_data);
+    xarr_free(gui->current_atlas.nodes);
     xarr_free(gui->rects);
+    xarr_free(gui->glyph_atlases);
 
     int error = FT_Done_Face(gui->ft_face);
     xassert(!error);
@@ -471,9 +564,13 @@ void pw_tick(void* _gui)
     // xassert(default_language != NULL);
     // hb_buffer_set_language(buf, default_language);
 
-    const char* my_text = "abc";
+    // https://utf8everywhere.org/
+    // const char* my_text = "abc";
     // const char* my_text = "Sphinx of black quartz, judge my vow";
     // const char* my_text     = "AV. .W.V.";
+    const char* my_text = "UTF8 Приве́т नमस्ते שָׁלוֹם 🐨";
+    // const char* my_text = "UTF8 Приве́т";
+
     size_t my_text_len = strlen(my_text);
     hb_buffer_add_utf8(buf, my_text, my_text_len, 0, my_text_len);
     // If we know the language:
@@ -505,16 +602,7 @@ void pw_tick(void* _gui)
         const hb_glyph_info_t*     info = glyph_info + i;
         const hb_glyph_position_t* pos  = glyph_pos + i;
 
-        atlas_rect* rect      = NULL;
-        const int   num_rects = xarr_len(gui->rects);
-        for (int j = 0; j < num_rects; j++)
-        {
-            if (gui->rects[j].header.glyphid == info->codepoint)
-            {
-                rect = gui->rects + j;
-                break;
-            }
-        }
+        atlas_rect* rect = get_glyph_rect(gui, info->codepoint);
 
         if (rect)
         {
@@ -576,6 +664,19 @@ void pw_tick(void* _gui)
     // IMG shader
     if (gui->num_indices)
     {
+        glyph_atlas* atlas = gui->glyph_atlases + gui->current_atlas.idx;
+        if (atlas->dirty)
+        {
+            sg_update_image(
+                atlas->img,
+                &(sg_image_data){
+                    .subimage[0][0] = {
+                        .ptr  = gui->current_atlas.img_data,
+                        .size = ATLAS_HEIGHT * ATLAS_ROW_STRIDE,
+                    }});
+            atlas->dirty = false;
+        }
+
         sg_range vert_range = {.ptr = gui->vertices, .size = sizeof(gui->vertices[0]) * gui->num_vertices};
         sg_range idx_range  = {.ptr = gui->indices, .size = sizeof(gui->indices[0]) * gui->num_indices};
         sg_update_buffer(gui->vbo, &vert_range);
@@ -583,11 +684,10 @@ void pw_tick(void* _gui)
 
         sg_apply_pipeline(gui->pip);
 
-        sg_bindings bind          = {0};
-        bind.vertex_buffers[0]    = gui->vbo;
-        bind.index_buffer         = gui->ibo;
-        bind.images[IMG_text_tex] = gui->img;
-        // bind.images[IMG_text_tex]   = gui->brain.img;
+        sg_bindings bind            = {0};
+        bind.vertex_buffers[0]      = gui->vbo;
+        bind.index_buffer           = gui->ibo;
+        bind.images[IMG_text_tex]   = atlas->img;
         bind.samplers[SMP_text_smp] = gui->smp;
 
         sg_apply_bindings(&bind);
